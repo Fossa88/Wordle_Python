674b8883462731d111c7bf3621727d2a7007b74b
Fossa88 <zedey700@gmail.com>
2025-04-19 16:42:03 -0700

    Implement confirm_word()
    Depending on the similarities of 2 words either a 2, 1 or a 0 is appended to a list the numbers each represent a certain condition which will be used to determine the colour of the boxes in draw_game()

--- wordle.py
+++ wordle.py
@@ -8,10 +8,10 @@ import random
 import display
 
 
-def validate_word(word, list_of_words):
+def validate_word(inputted_word, list_of_words):
     """
 
-    :param word: a string
+    :param inputted_word: a string
     :param list_of_words: a homogeneous list of strings
     :precondition: word has only 5 character
     :precondition: list_of_words only contains 5 letter strings
@@ -34,13 +34,13 @@ def validate_word(word, list_of_words):
     <BLANKLINE>
     False
     """
-    if word.lower() in list_of_words and len(word) == 5:
+    if inputted_word.lower() in list_of_words and len(inputted_word) == 5:
         return True
-    elif len(word) != 5:
-        print(f'\n{word} is not a 5-letter word!\n')
+    elif len(inputted_word) != 5:
+        print(f'\n{inputted_word} is not a 5-letter word!\n')
         return False
     else:
-        print(f'\n{word} is not a valid word!\n')
+        print(f'\n{inputted_word} is not a valid word!\n')
         return False
 
 
@@ -63,6 +63,47 @@ def select_word():
     return list_of_words
 
 
+def confirm_word(inputted_word, selected_word):
+    """
+
+    :param inputted_word: a string
+    :param selected_word: a string
+    :precondition: inputted_word and selected_word are both have
+                    only 5 characters
+    :postcondition: inputted_word and selected_word are compared for similarities of characters
+    :postcondition: a 2 is appended to a list if a character in inputted_word is the same
+                    and in the same spot in as selected_word
+    :postcondition: a 1 is appended to a list if a character in inputted_word is the same
+                    and is not the same spot in as selected_word
+    :postcondition: a 0 is appended to a list if a character in inputted_word is not inside
+                    selected_word
+    :return: A tuple of integers
+
+    >>> confirm_word('hello', 'hello')
+    (2, 2, 2, 2, 2)
+
+    >>> confirm_word('pizza', 'hello')
+    (0, 0, 0, 0, 0)
+
+    >>> confirm_word('porch', 'hello')
+    (0, 1, 0, 0, 1)
+    """
+    bool_list = []
+    counter = 0
+    for letter in inputted_word:
+        if letter in selected_word:
+            if letter == selected_word[counter]:
+                bool_list.append(2)
+                counter += 1
+            else:
+                bool_list.append(1)
+                counter += 1
+        else:
+            bool_list.append(0)
+            counter += 1
+    return tuple(bool_list)
+
+
 def game():
     """
     Drive the game
@@ -70,16 +111,19 @@ def game():
     play = display.intro()
     list_of_words = select_word()
     word = random.choice(list_of_words)
+    print(word)
     chosen_words = {1: '     ', 2: '     ', 3: '     ', 4: '     ', 5: '     '}
     counter = 1
-    display.draw_game(word, chosen_words)
+    display.draw_game(chosen_words)
     while play:
         user_input = input('Input a 5 letter word: ')
         validation = validate_word(user_input, list_of_words)
         if validation:
+            compared = confirm_word(user_input, word)
+            print(compared)
             chosen_words[counter] = user_input
             counter += 1
-            display.draw_game(word, chosen_words)
+            display.draw_game(chosen_words)
             if counter == 6:
                 play = False
     print('game over')
