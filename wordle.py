b8fff9404356ef2d18605ab3783a15827bd249a4
Fossa88 <zedey700@gmail.com>
2025-04-19 16:07:31 -0700

    Implement validate_word()
    Checks to see if a chosen word has 5-letters and is in the english language

--- wordle.py
+++ wordle.py
@@ -7,22 +7,60 @@ This shit is going to be so much harder than the new york times version because 
 import random
 import display
 
+
+def validate_word(word, list_of_words):
+    """
+
+    :param word: a string
+    :param list_of_words: a homogeneous list of strings
+    :precondition: word has only 5 character
+    :precondition: list_of_words only contains 5 letter strings
+    :postcondition: returns False if word is not 5 characters or inside list_of_words
+    :postcondition: returns True if word is 5 characters and inside list_of_words
+    :return: a bool True or False
+
+    >>> validate_word('hello', ['hello', 'pizza', 'bigot'])
+    True
+
+    >>> validate_word('hell', ['hello', 'pizza', 'bigot'])
+    <BLANKLINE>
+    hell is not a 5-letter word!
+    <BLANKLINE>
+    False
+
+    >>> validate_word('hopes', ['hello', 'pizza', 'bigot'])
+    <BLANKLINE>
+    hopes is not a valid word!
+    <BLANKLINE>
+    False
+    """
+    if word.lower() in list_of_words and len(word) == 5:
+        return True
+    elif len(word) != 5:
+        print(f'\n{word} is not a 5-letter word!\n')
+        return False
+    else:
+        print(f'\n{word} is not a valid word!\n')
+        return False
+
+
 def select_word():
     """
     Chose random word from words.txt
 
     :precondition: words.txt has strings inside
     :precondition: words.txt only has 5-letter words
-    :postcondition: word is a random 5-letter word from
-                    the english language
-    :return: word as a string of a 5-letter word
+    :postcondition: list_of_words has every 5-letter word in the
+                    english language
+    :return: list_of_words as a list with every 5-letter word
+             in the english language
     """
     list_of_words = []
     with open('words.txt') as file_object:
         for line in file_object:
-            list_of_words.append(line)
-    word = random.choice(list_of_words)
-    return word
+            cut_word = line[0:5]
+            list_of_words.append(cut_word)
+    return list_of_words
 
 
 def game():
@@ -30,14 +68,21 @@ def game():
     Drive the game
     """
     play = display.intro()
-    word = select_word()
+    list_of_words = select_word()
+    word = random.choice(list_of_words)
     chosen_words = {1: '     ', 2: '     ', 3: '     ', 4: '     ', 5: '     '}
     counter = 1
+    display.draw_game(word, chosen_words)
     while play:
-        display.draw_game(word, chosen_words)
-        user_input = input('Input a 5 letter word')
-        chosen_words[counter] = user_input
-        counter += 1
+        user_input = input('Input a 5 letter word: ')
+        validation = validate_word(user_input, list_of_words)
+        if validation:
+            chosen_words[counter] = user_input
+            counter += 1
+            display.draw_game(word, chosen_words)
+            if counter == 6:
+                play = False
+    print('game over')
 
 
 if __name__ == '__main__':
